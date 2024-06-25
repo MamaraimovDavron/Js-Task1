a42deb0d5f0fb93ad454570fa2dfc1b282a75885
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2024-06-25 16:33:27 +0500

    Js Task 1 done

---
+++ script.js
@@ -0,0 +1,31 @@
+// 1-masala
+
+/*let colorOne = "red";
+let colorTwo = "blue";
+
+colorOne = "blue";
+colorTwo = "red";
+
+console.log(colorOne);
+console.log(colorTwo);*/
+
+// 2-masala
+
+// let a = 7;
+// let b = 19;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(a);
+// console.log(b);
+
+// 3-masala
+
+var a = 10;
+var b = 20;
+
+(a = a + b), (b = a - b), (a = a - b);
+
+document.write("a = " + a + "</br> b = " + b);
